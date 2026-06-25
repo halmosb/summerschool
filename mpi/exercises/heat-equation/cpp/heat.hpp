40039a91fc5c2b9b1cc9b0c268f8b7c97656ee42
Balazs Paszkal Halmos <balazs.halmos@tuni.fi>
2026-06-25 15:27:57 +0300

    MPI calculations

--- mpi/exercises/heat-equation/cpp/heat.hpp
+++ mpi/exercises/heat-equation/cpp/heat.hpp
@@ -17,14 +17,21 @@ struct ParallelData {
 
       // TODO start: query number of MPI tasks and store it in
       // the size attribute of the class
-
+      MPI_Comm_size(MPI_COMM_WORLD, &size);
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
       // Query MPI rank of this task and store it in the rank attribute
       // Determine also up and down neighbours of this domain and store
       // them in nup and ndown attributes, remember to cope with
       // boundary domains appropriatly
 
-      nup =
-      ndown =
+      nup = rank - 1; 
+      ndown = rank + 1;
+      if (nup < 0){
+      	nup = MPI_PROC_NULL;
+      }
+      if (ndown > size - 1){
+      	ndown = MPI_PROC_NULL;
+      }
 
       // TODO end
 
